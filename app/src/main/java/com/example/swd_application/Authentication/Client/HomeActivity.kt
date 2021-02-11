e545527c82950ee2e3d509249f8c18ed9fdac3aa
Avinash Vijayvargiya <avinash14022002@gmail.com>
2021-02-12 00:24:57 +0530

    Profile page with image glide view

--- app/src/main/java/com/example/swd_application/Authentication/Client/HomeActivity.kt
+++ app/src/main/java/com/example/swd_application/Authentication/Client/HomeActivity.kt
@@ -1,12 +1,23 @@
 package com.example.swd_application.Authentication.Client
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.swd_application.Authentication.Admin.AdminLoginActivity
 import com.example.swd_application.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+
+        val btn_profile = findViewById<Button>(R.id.btn_profile)
+
+        btn_profile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
