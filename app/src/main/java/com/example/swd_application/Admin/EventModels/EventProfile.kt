548658f816222e3d6d1cecf1324f9fd12f8910ee
Anubhav Pabby <legendtheanubhav@gmail.com>
2021-02-18 13:03:38 +0530

    Implemented Admin Side Event Form Work

---
+++ app/src/main/java/com/example/swd_application/Admin/EventModels/EventProfile.kt
@@ -0,0 +1,17 @@
+package com.example.swd_application.Admin
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class EventProfile(
+    val EventConductedYear:Int,
+    val EventDescription: String,
+    val EventEndDate: String?,
+    val EventFlagship: Boolean,
+    val EventHeads: List<String>?,
+    val EventImageUrl: String?,
+    val EventLinkUrl: String,
+    val EventName: String,
+    val EventStartDate: String?
+)
+
