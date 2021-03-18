6ed9b3f4b3d92a8f2d36b60876cd3206ee042f44
Avinash Vijayvargiya <avinash14022002@gmail.com>
2021-03-18 15:18:33 +0530

    Added: register and login UI

---
+++ app/src/main/java/com/example/swd_application/User/Activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.swd_application.User.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.swd_application.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_new)
+    }
+}
---
+++ app/src/main/java/com/example/swd_application/User/Activities/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.swd_application.User.Activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.swd_application.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
